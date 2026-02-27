093f88c795b55f056d0f49dd1041af6da7aa86bd
Apurv Gupta <186622906+Apurv7Gupta@users.noreply.github.com>
2026-02-27 18:45:19 +0530

    Create Basics.java

---
+++ Java/Basics.java
@@ -0,0 +1,29 @@
+public class FileName {
+
+    // main function
+
+    public static void main(String[] args) {
+    }
+
+    // Class Definition
+
+    static class ClassName {
+        public int a;
+        public String b;
+
+        void Method() {
+        }
+    }
+
+}
+
+/// The most outer class' name should match the filename exactly
+
+
+/// Main Function -
+
+// This is the entry point of the application
+// "main" is only special if it matches the required signature
+// Otherwise it’s just another method
+// The parameters "String[]" and "args" are for running commandline commands by storing them in the String[] array 
+// "args" can be named anything
