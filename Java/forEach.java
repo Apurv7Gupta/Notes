0df5878c734c803c59f97336cf903a564c7bed7c
Apurv Gupta <186622906+Apurv7Gupta@users.noreply.github.com>
2026-03-01 04:39:04 +0530

    Create forEach.java

---
+++ Java/forEach.java
@@ -0,0 +1,9 @@
+public class forEach {
+    public static void main(String[] ParameterName) {
+        int[] numbers = { 1, 2, 3, 4, 5 };
+        for (int num : numbers) {
+            System.out.print(num + " and ");
+        }
+        // Output: 1 2 3 4 5
+    }
+}
