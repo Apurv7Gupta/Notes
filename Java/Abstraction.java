bca256470f5bc0411a2a4d1f8efac91cc5b64df9
Apurv Gupta <186622906+Apurv7Gupta@users.noreply.github.com>
2026-03-04 19:27:08 +0530

    Update Abstraction.java

--- Java/Abstraction.java
+++ Java/Abstraction.java
@@ -65,3 +65,14 @@ public class Abstraction {
 
 // At compile time → both classes are typed as Animal.
 // At runtime → Java dispatches to the actual object (Cat or Seagull).
+
+// ---------- Interface vs Abstract classes ----------
+
+// 1) You can implement as many Interfaces as you want in the child class, but you can extend only one Abstract class in a child class
+// 2) You can define which method wil be abstract in an Abstract class, in an Interface, every method is abstract, unless its default
+
+// 3) You use "Extends" to extend an Abstract class, you use "Implements" to implement an Interface
+
+// 4) You can change fields in Abstract classes, in Interfaces, every field is static and final
+
+// 5) Abstract classes can have concrete methods freely; interfaces only via default/static
